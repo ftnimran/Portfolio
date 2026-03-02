83dffe32d2d5d0d054d877a1be62abefe3ce087b
Imran Ali <109467587+ftnimran@users.noreply.github.com>
2026-03-02 23:52:33 +0530

    formating all tag

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,31 +5,33 @@ import PillButtons from "../components/PillButtons.jsx";
 
 const About = () => {
   return (
-    <section id="about" className="w-full min-h-screen pt-17.5 px-[5%] md:px-[10%] pb-0">
+    <section
+      id="about"
+      className="w-full min-h-screen pt-17.5 px-[5%] md:px-[10%] pb-0 *:leading-tight"
+    >
       <SectionTitle titleText1="About" titleText2=" Me" />
 
-      <div className="flex flex-col md:flex-row items-center justify-between relative mt-12 md:mt-17 w-full h-full">
-        
+      <div className="flex flex-col md:flex-row items-center justify-between relative mt-8 md:mt-20 w-full h-full">
         {/* Profile Image Container */}
         <div className="about-picture">
-          <img 
-            src={backGround2} 
-            alt="Imran Ali" 
+          <img
+            src={backGround2}
+            alt="Imran Ali"
             className="h-52 w-52 md:h-100 md:w-100 rounded-full object-cover shadow-[0_0_20px_#0ef]"
           />
         </div>
 
         {/* Text Content Container */}
         <div className="max-w-full md:max-w-150 w-full">
-          <h1 className="text-[2.35rem] md:text-[56px] font-bold  pt-5 md:pt-0 pb-2.5 md:pb-2">
+          <h1 className="text-[2.35rem] md:text-[56px] font-bold  pt-4 md:pt-0 pb-1.5 md:pb-2 -my-0.75">
             I'm <span className="text-[#0ef]">Imran Ali</span>
           </h1>
-          
-          <h5 className="text-[1.35rem] md:text-[32px] font-bold pb-2 md:pb-1">
+
+          <h5 className="text-[1.35rem] md:text-[32px] font-bold pb-1.5 md:pb-1">
             MERN Stack Developer
           </h5>
-          
-          <p className="text-[1.1rem] md:text-[20px] leading-relaxed font-medium pb-4">
+
+          <p className="text-[1.1rem] md:text-[20px]  font-medium pb-3">
             I'm a passionate MERN Stack Developer who loves building modern,
             responsive, and high-performance web applications. I work with
             MongoDB, Express.js, React, and Node.js to create seamless
@@ -37,12 +39,11 @@ const About = () => {
             is on writing clean, scalable, and efficient code with attention to
             design and functionality.
           </p>
-          
-          <div className="mt-7 md:mt-9">
+
+          <div className="mt-3 md:mt-4.5">
             <PillButtons btnText="More About Me" url="#about" />
           </div>
         </div>
-
       </div>
     </section>
   );
